10241128a4ce6e059a3c20305e1938dc169b67ef
ilsaf <justisarex@gmail.com>
2023-01-22 13:16:10 +0300

    update async, threads and processes

--- sync-async-threads-processes.py
+++ sync-async-threads-processes.py
@@ -4,59 +4,67 @@ import concurrent.futures
 import aiohttp
 import asyncio
 from asyncio import Semaphore
+from aiofile import AIOFile
 
 URL = 'https://loremflickr.com/320/240'
-COUNT_IMAGES = 2000
+COUNT_IMAGES = 100
 COUNT_THREADS = 15
 COUNT_PROCESSES = 5
 COUNT_ASYNC = 100
 
 
-def download_image(number_image, method):
-    print(f'Началась загрузка картинки №{number_image}\n')
-    response = requests.get(URL, allow_redirects=True)
-    filename = f"{method}/{int((time())*1000)}.jpeg"
-    with open(filename, 'wb') as file:
-        file.write(response.content)
-        print(f'Закончилась загрузка картинки №{number_image}\n')
+class Downloader:
+    def __init__(self, method_str):
+        self.method_str = method_str
+
+    def download_image(self, number_image):
+        """This method for sync functions"""
+        print(f'Началась загрузка картинки №{number_image}\n')
+        response = requests.get(URL, allow_redirects=True)
+        filename = f"{self.method_str}/{int((time()) * 1000)}.jpeg"
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+            print(f'Закончилась загрузка картинки №{number_image}\n')
+
+    async def async_download_image(self, semaphore, url, session, number_image):
+        """This method for async functions"""
+        await semaphore.acquire()
+        print(f'Начинается скачивание {number_image}')
+        async with session.get(url, allow_redirects=True) as r:
+            filename = f'{self.method_str}/file-{int(time() * 1000)}.jpeg'
+            async with AIOFile(filename, 'wb') as f:
+                data = await r.read()
+                await f.write(data)
+                await f.fsync()
+                print(f'Заканчивается скачивание {number_image}')
+            semaphore.release()
 
 
 def sync():
+    downloader = Downloader('sync')
     for number_image in range(COUNT_IMAGES):
-        download_image(number_image, method='sync')
+        downloader.download_image(number_image)
 
 
 def thread():
+    downloader = Downloader('thread')
     with concurrent.futures.ThreadPoolExecutor(COUNT_THREADS) as executor:
-        futures = []
-        for i in range(COUNT_IMAGES):
-            futures.append(executor.submit(download_image, number_image=i, method='thread'))
+        executor.map(downloader.download_image, range(COUNT_IMAGES))
 
 
 def processes():
+    downloader = Downloader('processes')
     with concurrent.futures.ProcessPoolExecutor(COUNT_PROCESSES) as executor:
-        futures = []
-        for i in range(COUNT_IMAGES):
-            futures.append(executor.submit(download_image, number_image=i, method='processes'))
-
-
-async def async_download_image(semaphore, url, session, number_image):
-    await semaphore.acquire()
-    print(f'Начинается скачивание {number_image}')
-    async with session.get(url, allow_redirects=True) as response:
-        filename = f'async/file-{int(time() * 1000)}.jpeg'
-        with open(filename, 'wb') as file:
-            file.write(await response.read())
-        print(f'Заканчивается скачивание {number_image}')
-        semaphore.release()
+        executor.map(downloader.download_image, range(COUNT_IMAGES))
 
 
 async def async_function():
+    downloader = Downloader(method_str='async')
     tasks = list()
     semaphore = Semaphore(COUNT_ASYNC)
     async with aiohttp.ClientSession() as session:
         for number_image in range(COUNT_IMAGES):
-            task = asyncio.create_task(async_download_image(semaphore, URL, session, number_image))
+            task = asyncio.create_task(downloader.async_download_image(semaphore, URL, session, number_image))
             tasks.append(task)
         await asyncio.gather(*tasks)
 
